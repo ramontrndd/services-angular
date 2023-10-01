70335360742742bcd025cffd91fb93d17b6987d5
unknown <ramonbraintrindade@gmail.com>
2023-10-01 13:20:08 -0300

    learning services with angular

---
+++ service-angular/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <h1>Aula de Services</h1><router-outlet></router-outlet><app-card></app-card>`,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'service-angular';
+}

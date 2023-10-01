8913c71135f14500c9228e392e90531e4fba1fc1
unknown <ramonbraintrindade@gmail.com>
2023-10-01 15:23:22 -0300

    learning service finish

--- service-angular/src/app/components/card/card.component.ts
+++ service-angular/src/app/components/card/card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PokemonData } from 'src/app/models/pokemonData';
 import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
@@ -7,15 +8,39 @@ import { PokemonService } from 'src/app/services/pokemon.service';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
-  name:string = "Bulbassaur"
-  attributesTypes:string[] = ['FIRE', 'ROCK']
+  pokemon:PokemonData
 
   constructor(
     private service:PokemonService
-  ) { }
+  ) { 
+    this.pokemon = {
+      id:0,
+      name:'',
+      sprites:{
+        front_default:''
+      },
+      types:[]
+    }
+  }
 
   ngOnInit(): void {
-    this.service.getPokemon("bulbasaur")
+    this.getPokemon('bulbasaur')
+  }
+  getPokemon(searchName:string){
+    this.service.getPokemon(searchName).subscribe(
+      {
+        next: (res) =>{
+          
+          this.pokemon = {
+            id: res.id,
+            name: res.name,
+            sprites: res.sprites,
+            types: res.types
+          }
+          },
+        error: (err) => console.log('not found')
+      }
+    )
   }
 
 }
